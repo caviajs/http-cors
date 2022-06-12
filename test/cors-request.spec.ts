8d97889e4cd822edd002ec8e8eb764dfa635b4fa
Paweł Partyka <partyka95@icloud.com>
2022-06-12 17:05:30 +0200

    test: cors request

---
+++ test/cors-request.spec.ts
@@ -0,0 +1,37 @@
+import { HttpRouter } from '@caviajs/http-router';
+import { HttpCors } from '../src';
+import http from 'http';
+import supertest from 'supertest';
+
+it('should add CORS-request headers and execute handler', async () => {
+  const httpRouter: HttpRouter = new HttpRouter();
+
+  httpRouter
+    .intercept(HttpCors.setup({
+      'Access-Control-Allow-Credentials': true,
+      'Access-Control-Allow-Headers': ['X-Foo'],
+      'Access-Control-Allow-Methods': ['GET'],
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Expose-Headers': ['Y-Foo'],
+      'Access-Control-Max-Age': 500,
+    }))
+    .route({ handler: () => 'Hello Cavia', method: 'GET', path: '/' });
+
+  const httpServer: http.Server = http.createServer((request, response) => {
+    httpRouter.handle(request, response);
+  });
+
+  const response = await supertest(httpServer)
+    .get('/')
+    .set('Origin', 'https://caviajs.com');
+
+  expect(response.headers['access-control-allow-credentials']).toBe('true'); // CORS-request header
+  expect(response.headers['access-control-allow-headers']).toBeUndefined();
+  expect(response.headers['access-control-allow-methods']).toBeUndefined();
+  expect(response.headers['access-control-allow-origin']).toBe('*'); // CORS-request header
+  expect(response.headers['access-control-expose-headers']).toBe('Y-Foo'); // CORS-request header
+  expect(response.headers['access-control-max-age']).toBeUndefined();
+  expect(response.headers['vary']).toBe('Origin'); // CORS-request header
+  expect(response.statusCode).toBe(200);
+  expect(response.text).toBe('Hello Cavia');
+});
